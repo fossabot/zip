9485d9d0db163110d56f41978b615fc76d112291
Zipdot <zipdot.me@gmail.com>
2020-12-09 13:50:45 +0700

    Create mix project

---
+++ lib/bejo.ex
@@ -0,0 +1,5 @@
+defmodule Bejo do
+  def start do
+    "Hello world"
+  end
+end
---
+++ test/bejo_test.exs
@@ -0,0 +1,7 @@
+defmodule BejoTest do
+  use ExUnit.Case
+
+  test "greets the world" do
+    assert Bejo.start() == "Hello world"
+  end
+end
